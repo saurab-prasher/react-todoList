711d5030b5ed6dc5d3573dfb306e6798b9b91f0f
Saurab Prasher <saurab015@gmail.com>
2021-02-12 18:42:42 +0530

    deleting functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,29 @@ import React, { useState } from "react";
 import Todo from "./Todo";
 import Header from "./Header";
 import InputBar from "./InputBar";
+import { nanoid } from "nanoid";
 
 const App = (props) => {
   const [tasks, setTasks] = useState(props.tasks);
 
-  const AddTask = (name) => {
-    const newTask = { id: "id", name: name };
+  const addTask = (name) => {
+    const newTask = { id: `todo-${nanoid()}`, name: name };
     setTasks([...tasks, newTask]);
   };
-  const taskList = tasks.map((task) => <Todo key={task.id} name={task.name} />);
+  const deleteTask = (id) => {
+    const remainingTask = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTask);
+  };
+  const taskList = tasks.map((task) => (
+    <Todo key={task.id} id={task.id} name={task.name} deleteTask={deleteTask} />
+  ));
 
   return (
     <>
       {/* Header */}
       <Header />
       {/* Add the task InputBar */}
-      <InputBar onSubmit={AddTask} />
+      <InputBar onSubmit={addTask} />
 
       {/* View Template */}
       <div className=" mt-5 p-6 bg-indigo-300 max-w-xl mx-auto shadow-sm rounded-xl  ">
@@ -34,7 +41,7 @@ const App = (props) => {
           <div className="btn-group">
             <button
               type="button"
-              className="ml-1 bg-gray-400 text-white  px-1.5 py-1.5 rounded-md  focus:outline-none"
+              className="ml-1 bg-gray-400 text-white px-1.5 py-1.5 rounded-md  focus:outline-none"
             >
               Cancel
             </button>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Todo = ({ name }) => {
+const Todo = ({ name, deleteTask, id }) => {
   return (
     <div className="flex items-center">
-      <li className="bg-white w-max p-2 rounded-md my-2 ">{name}</li>
+      <li id={id} className="bg-white w-max p-2 rounded-md my-2 ">
+        {name}
+      </li>
       <div className="btn-group">
         <button
           type="button"
@@ -14,6 +16,7 @@ const Todo = ({ name }) => {
         <button
           type="button"
           className="ml-1 bg-red-600 px-1.5 py-1.5 text-white rounded-md focus:outline-none"
+          onClick={() => deleteTask(id)}
         >
           Delete
         </button>
