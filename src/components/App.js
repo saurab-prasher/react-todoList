1ee4850c78bfe3ec6defdfd33c6fbb5b5a0a3fc0
Saurab Prasher <saurab015@gmail.com>
2021-02-12 19:09:39 +0530

    editing func added

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,23 @@ const App = (props) => {
     const remainingTask = tasks.filter((task) => id !== task.id);
     setTasks(remainingTask);
   };
+
+  const editTask = (id, newName) => {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) return { ...task, name: newName };
+      return task;
+    });
+
+    setTasks(editedTaskList);
+  };
   const taskList = tasks.map((task) => (
-    <Todo key={task.id} id={task.id} name={task.name} deleteTask={deleteTask} />
+    <Todo
+      key={task.id}
+      id={task.id}
+      name={task.name}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
   ));
 
   return (
@@ -30,29 +45,6 @@ const App = (props) => {
       <div className=" mt-5 p-6 bg-indigo-300 max-w-xl mx-auto shadow-sm rounded-xl  ">
         <ul className="list-none">{taskList}</ul>
         {/* Edit Template */}
-        <form className="flex my-2 items-center">
-          <div className="form-group">
-            <input
-              className="focus:outline-none rounded-md p-2 shadow-sm placeholder-gray-500"
-              type="text"
-              id="name"
-            />
-          </div>
-          <div className="btn-group">
-            <button
-              type="button"
-              className="ml-1 bg-gray-400 text-white px-1.5 py-1.5 rounded-md  focus:outline-none"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="ml-1 px-1.5 py-1.5 rounded-md text-white bg-green-500 focus:outline-none"
-            >
-              Save
-            </button>
-          </div>
-        </form>
       </div>
     </>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Todo = ({ name, deleteTask, id }) => {
-  return (
+const Todo = ({ name, deleteTask, id, editTask }) => {
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState("");
+
+  const onInputChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    editTask(id, newName);
+    setNewName("");
+    setEditing(false);
+  };
+
+  const editingTemplate = (
+    <form onSubmit={onFormSubmit} className="flex my-2 items-center">
+      <div className="form-group">
+        <input
+          className="focus:outline-none rounded-md p-2 shadow-sm placeholder-gray-500"
+          type="text"
+          id="name"
+          onChange={onInputChange}
+          value={newName}
+        />
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="ml-1 bg-gray-400 text-white px-1.5 py-1.5 rounded-md  focus:outline-none"
+          onClick={() => setEditing(false)}
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="ml-1 px-1.5 py-1.5 rounded-md text-white bg-green-500 focus:outline-none"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
+
+  const viewTemplate = (
     <div className="flex items-center">
       <li id={id} className="bg-white w-max p-2 rounded-md my-2 ">
         {name}
@@ -9,7 +52,8 @@ const Todo = ({ name, deleteTask, id }) => {
       <div className="btn-group">
         <button
           type="button"
-          className="ml-1 bg-yellow-400 px-1.5 py-1.5 text-gray-600 text-white rounded-md focus:outline-none "
+          className="ml-1 bg-yellow-400 px-1.5 py-1.5 text-gray-600 text-white rounded-md focus:outline-none"
+          onClick={() => setEditing(true)}
         >
           Edit
         </button>
@@ -23,6 +67,8 @@ const Todo = ({ name, deleteTask, id }) => {
       </div>
     </div>
   );
+
+  return isEditing ? editingTemplate : viewTemplate;
 };
 
 export default Todo;
