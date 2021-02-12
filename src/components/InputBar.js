5c55e8647e18338adb8e3e70b73a27a7fb9ea1e0
Saurab Prasher <saurab015@gmail.com>
2021-02-12 19:50:54 +0530

    functionality completed

--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const InputBar = ({ onSubmit }) => {
   const [name, setName] = useState("");
+
+  const searchFieldRef = useRef(null);
+
+  useEffect(() => {
+    searchFieldRef.current.focus();
+  }, []);
 
   const onFormSubmit = (e) => {
     e.preventDefault();
@@ -16,14 +22,15 @@ const InputBar = ({ onSubmit }) => {
   return (
     <form onSubmit={onFormSubmit} className="max-w-xl mx-auto mt-5">
       <input
+        ref={searchFieldRef}
         type="text"
-        className="focus:outline-none rounded-md p-2 shadow-sm placeholder-gray-500"
+        className="rounded-md p-2 shadow-sm placeholder-gray-500"
         placeholder="Enter the task here"
         value={name}
         onChange={onInputChange}
       />
       <button
-        className="px-2.5 py-1.5 bg-green-400 text-white rounded-md focus:outline-none shadow-sm ml-1"
+        className="px-2.5 py-1.5 bg-green-400 text-white rounded-md shadow-sm ml-1"
         type="submit"
       >
         Add
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 const Todo = ({ name, deleteTask, id, editTask }) => {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState("");
+
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
+
+  useEffect(() => {
+    if (!wasEditing && isEditing) editFieldRef.current.focus();
+    if (wasEditing && !isEditing) editButtonRef.current.focus();
+  }, [wasEditing, isEditing]);
 
   const onInputChange = (e) => {
     setNewName(e.target.value);
@@ -10,6 +28,7 @@ const Todo = ({ name, deleteTask, id, editTask }) => {
 
   const onFormSubmit = (e) => {
     e.preventDefault();
+    if (!newName) return;
     editTask(id, newName);
     setNewName("");
     setEditing(false);
@@ -18,25 +37,27 @@ const Todo = ({ name, deleteTask, id, editTask }) => {
   const editingTemplate = (
     <form onSubmit={onFormSubmit} className="flex my-2 items-center">
       <div className="form-group">
+        <label>{name.charAt(0).toUpperCase() + name.slice(1)} : </label>
         <input
-          className="focus:outline-none rounded-md p-2 shadow-sm placeholder-gray-500"
+          className=" rounded-md p-2 shadow-sm placeholder-gray-500"
           type="text"
           id="name"
           onChange={onInputChange}
           value={newName}
+          ref={editFieldRef}
         />
       </div>
       <div className="btn-group">
         <button
           type="button"
-          className="ml-1 bg-gray-400 text-white px-1.5 py-1.5 rounded-md  focus:outline-none"
+          className="ml-1 bg-gray-400 text-white px-1.5 py-1.5 rounded-md  "
           onClick={() => setEditing(false)}
         >
           Cancel
         </button>
         <button
           type="submit"
-          className="ml-1 px-1.5 py-1.5 rounded-md text-white bg-green-500 focus:outline-none"
+          className="ml-1 px-1.5 py-1.5 rounded-md text-white bg-green-500 "
         >
           Save
         </button>
@@ -52,14 +73,15 @@ const Todo = ({ name, deleteTask, id, editTask }) => {
       <div className="btn-group">
         <button
           type="button"
-          className="ml-1 bg-yellow-400 px-1.5 py-1.5 text-gray-600 text-white rounded-md focus:outline-none"
+          className="ml-1 bg-yellow-400 px-1.5 py-1.5 text-gray-600 text-white rounded-md "
           onClick={() => setEditing(true)}
+          ref={editButtonRef}
         >
           Edit
         </button>
         <button
           type="button"
-          className="ml-1 bg-red-600 px-1.5 py-1.5 text-white rounded-md focus:outline-none"
+          className="ml-1 bg-red-600 px-1.5 py-1.5 text-white rounded-md "
           onClick={() => deleteTask(id)}
         >
           Delete
